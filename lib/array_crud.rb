b4fdddd578f1695fed4fdc94b238052f10634615
Andrew Smith <848smith@gmail.com>
2020-01-29 06:04:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  phrase << "arrays!"
+  create_an_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  phrase.unshift("wow")
+  
 end
 
 def remove_element_from_end_of_array(array)
